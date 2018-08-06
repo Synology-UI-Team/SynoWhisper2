2b3e0c5c17645f0902540b7e144526d95ae72952
root <root@DESKTOP-FS4C3QC.localdomain>
2018-08-07 00:02:59 +0800

    Initial Projects

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+			},
+			{
+				test: /\.s[a|c]ss$/,
+				loader: 'style-loader!css-loader!sass-loader'
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.js', '.vue', '.json'],
+		alias: {
+			'vue$': 'vue/dist/vue.esm.js'
+		}
+	},
+	devServer: {
+		publicPath: '/dist/'
+	},
+	entry: ['./src/main.js'],
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js'
+	},
+	plugins: [
+		new VueLoaderPlugin()
+	]
+};
